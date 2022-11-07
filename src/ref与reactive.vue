295d3b850700c88b683e4ef236be100f8cbc6a7b
A313117418 <313117418@qq.com>
2022-11-08 03:26:17 +0800

    提交

---
+++ src/ref与reactive.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        {{msg.name}}
+    </div>
+    <p>{{money}}</p>
+    <button @click="eventDiji">点击</button>
+</template>
+<script lang="ts">
+import { defineComponent ,reactive,ref} from 'vue';
+
+export default defineComponent({
+    setup(){
+        let msg = reactive({
+            name:"zhangsan",
+            age:18
+        });
+        let money = ref(1000)
+        let eventDiji = ()=>{
+            msg.name="nest",
+            money.value = 200
+        }
+        return {msg ,eventDiji,money}
+    }
+})
+</script>
